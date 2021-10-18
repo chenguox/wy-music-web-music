ce4ed4585db977aa0ceddb58225d576adea00765
“陈国鑫” <529488618@qq.com>
2021-10-18 18:03:55 +0800

    11

---
+++ src/store/index.js
@@ -0,0 +1,3 @@
+
+// 使用 redux-thunk 中间件，改造 store.dispatch, 使得后者可以接受函数作为参数
+import thunk from 'redux-thunk';
---
+++ src/store/reducer.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+
+import reducer from './reducer';
+
+// 创建 store,需要接收一个 reducer
+const store = createStore()
