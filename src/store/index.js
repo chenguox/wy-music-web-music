e8facee21ec17e315d82b8667410f499f4f1bb13
“陈国鑫” <529488618@qq.com>
2021-12-01 17:53:59 +0800

    redux配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
-
+import { createStore, applyMiddleware, compose } from 'redux';
 // 使用 redux-thunk 中间件，改造 store.dispatch, 使得后者可以接受函数作为参数
 import thunk from 'redux-thunk';
+import reducer from './reducer';
+// 在 redux 中集成 devtools 的中间件
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// 创建 store 
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+export default store;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 
-import reducer from './reducer';
+// 在对应组件下创建对应的文件
+import { reducer as recommendReducer } from '../pages/discover/c-pages/recommend/store';
+import { reducer as playerReducer } from '../pages/player/store';
 
-// 创建 store,需要接收一个 reducer
-const store = createStore()
+// Reducer 函数负责生成 State。由于整个应用只有一个 State 对象， 包含所有数据，
+// 那么这个 state 必然十分庞大，导致 Reducer 函数也十分庞大，
+// 所以可以把 Reducer 函数拆分。不同的函数负责处理不同属性，
+// 最终把它们合并成一个大的 Reducer 即可
+const cReducer = combineReducers({
+  recommend: recommendReducer,
+  player: playerReducer
+});
+
+export default cReducer;
